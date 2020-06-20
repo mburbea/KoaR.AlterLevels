da2c44236db98f9c147e94c83dbb4732cd5b0d6f
Michael Burbea <michaelburbea@gmail.com>
2020-06-20 00:57:30 -0400

    Add project files.

---
+++ KoaR.AlterLevels/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace KoaR.AlterLevels
+{
+    static class Program
+    {
+        private readonly static Dictionary<uint, string> _simSpaces =
+            File.ReadLines("simspace.csv")
+            .Skip(1)
+            .ToDictionary(x => uint.Parse(x[..6], NumberStyles.HexNumber), x => x[7..]);
+
+        static List<int> GetAllIndices(ReadOnlySpan<byte> data, ReadOnlySpan<byte> sequence)
+        {
+            var results = new List<int>();
+            int ix = data.IndexOf(sequence);
+            int start = 0;
+
+            while (ix != -1)
+            {
+                results.Add(start + ix - 3); // get the sim space
+                start += ix + sequence.Length;
+                ix = data.Slice(start).IndexOf(sequence);
+            }
+            return results;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Argument error, usage: path to save followed by level");
+                return;
+            }
+            var path = args[0];
+            if (!int.TryParse(args[1], out int level))
+            {
+                Console.WriteLine($"Could not parse {args[1]} as Byte");
+                return;
+            }
+
+            var saveData = File.ReadAllBytes(path);
+            ReadOnlySpan<byte> sequence = new byte[] { 0x00, 0x89, 0xFB, 0x40, 0x01, 0x03 };
+            ReadOnlySpan<byte> allOnes = new byte[] { 0x1, 0x1, 0x1, 0x1 };
+            var spaces = GetAllIndices(saveData, sequence);
+            foreach (var spaceIx in spaces)
+            {
+                var id = BitConverter.ToUInt32(saveData, spaceIx);
+                if (!_simSpaces.TryGetValue(id, out var name))
+                {
+                    continue;
+                }
+                var dataLength = BitConverter.ToInt32(saveData, spaceIx + 9);
+                Span<byte> span = saveData.AsSpan(spaceIx + 13, dataLength);
+                var ixOfAllOnes = span.IndexOf(allOnes);
+                if (ixOfAllOnes <= 11)
+                {
+                    continue;
+                }
+                var levelSpan = span.Slice(ixOfAllOnes - 12, 4);
+                var oldLevel = MemoryMarshal.Read<int>(levelSpan);
+                if ((uint)oldLevel < 255u)
+                {
+                    MemoryMarshal.Write(levelSpan, ref level);
+                    Console.WriteLine($"Space:{name} oldLvl:{oldLevel} newLvl:{level}");
+                }
+            }
+            Console.WriteLine($"Creating a backup: ${path}.bak");
+            File.Copy(path, path + ".bak", overwrite: true);
+            File.WriteAllBytes(path, saveData);
+        }
+    }
+}
